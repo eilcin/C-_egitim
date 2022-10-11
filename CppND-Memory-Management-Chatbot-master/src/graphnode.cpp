00944bb07799772dc691671ec267d2ed2c6e946b
eilcin <103742151+eilcin@users.noreply.github.com>
2022-10-11 21:14:14 +0300

    Memory Management added

---
+++ CppND-Memory-Management-Chatbot-master/src/graphnode.cpp
@@ -0,0 +1,60 @@
+#include "graphedge.h"
+#include "graphnode.h"
+#include <iostream>
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    std::cout << "GraphNode Constructor" << std::endl;
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    _childEdges.push_back(edge);
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot &&chatbot)
+{   //Move the object 
+ _chatBot = std::move(chatbot);
+ _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(std::move(_chatBot));
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
